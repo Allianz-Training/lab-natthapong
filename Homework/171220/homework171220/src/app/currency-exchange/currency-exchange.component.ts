88f64825dc568df18cb88dab442e49e027f347a5
Natthapong <natthapong41@gmail.com>
2020-12-18 11:26:48 +0700

    Add hw2

--- Homework/171220/homework171220/src/app/currency-exchange/currency-exchange.component.ts
+++ Homework/171220/homework171220/src/app/currency-exchange/currency-exchange.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-currency-exchange',
   templateUrl: './currency-exchange.component.html',
-  styleUrls: ['./currency-exchange.component.css']
+  styleUrls: ['./currency-exchange.component.css'],
 })
 export class CurrencyExchangeComponent implements OnInit {
+  @Input()
+  sourceCurrency: string = '';
+  destinationCurrency: string = '';
+  amount: number;
+  result: number;
+  rate: number;
 
-  constructor() { }
+  constructor(private client: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  convert(): void {
+    if (
+      this.sourceCurrency === '' ||
+      this.destinationCurrency === '' ||
+      this.amount === undefined
+    ) {
+      alert('PLEASE ENTER EVERY BOX');
+    } else if (this.amount <= 0) {
+      alert('Invalid number: positive only');
+    } else {
+      this.request();
+    }
   }
 
+  request() {
+    this.client
+      .get(
+        `https://api.exchangeratesapi.io/latest?base=${this.sourceCurrency.toUpperCase()}&symbols=${this.destinationCurrency.toUpperCase()}`
+      )
+      .subscribe(
+        (res) => {
+          this.rate = res['rates'][this.destinationCurrency.toUpperCase()];
+          this.result = this.amount * this.rate;
+        },
+        (err) => {
+          alert(err.status + ' Invalid input');
+        }
+      );
+  }
 }
